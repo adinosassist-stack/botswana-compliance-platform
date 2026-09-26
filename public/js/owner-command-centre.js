b929a1f152f0ac74fc759114c1431b1024b69800
adinosassist-stack <adinosassist@gmail.com>
2026-09-26 16:56:06 +0200

    fix(ui): preserve compatible owner command centre release marker

--- public/js/owner-command-centre.js
+++ public/js/owner-command-centre.js
@@ -1,7 +1,7 @@
 (function initOwnerCommandCentre(global){
   "use strict";
 
-  const RELEASE="20260926-v157";
+  const RELEASE="20260913d";
   const MAX_OPPORTUNITIES=500;
   const MAX_CAMPAIGNS=50;
   const PROFILE_KEYS=Object.freeze({
